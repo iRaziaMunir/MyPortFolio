8ab3d2d35b02b0349c8de0573756b6373e9e7bfa
iRaziMunir <80644602+iRaziaMunir@users.noreply.github.com>
2024-05-21 14:04:07 +0500

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,11 +10,8 @@ const Hero = () => {
   const texts = ["Hello, world!", "Welcome to React!", "Enjoy Typewriter Effect!"];
 
   return (
-<<<<<<< HEAD:src/components/Header.jsx
-    <section className="w-full py-40 px-4 md:px-12 lg:px-0 flex items-center rounded-md ">
-=======
+   
     <section id='hero' className="w-full lg:py-40 py-4 px-4 md:px-12 lg:px-0 flex items-center rounded-md oddSection-bg-color">
->>>>>>> 19a50227fe9c55dfd3d073945bdb7908c4e26b23:src/components/Hero.jsx
         <div className="w-full flex flex-col md:flex-row items-center justify-center">
           {/* Text Section */}
           <div className=" w-full md:w-1/2 lg:w-2/2 md:pr-8 lg:pr-12 mb-8 md:mb-0">
@@ -28,15 +25,9 @@ const Hero = () => {
               I am <span className='font-semibold text-[#ffc100] hover:text-[#df1edc] hover:transition-all hover:ease-in-out hover:duration-700 uppercase motion-safe:animate-pulse drop-shadow-5xl'> Razia Munir!</span><br />
               {/* <span className='hover:text-[#6b6d34] text-[#701219] tracking-wide'>Frontend Developer</span>  */}
             </p>
-<<<<<<< HEAD:src/components/Header.jsx
-            <h1 className="text-[4vw] font-semibold uppercase opacity-60 leading-none ml-8 lg:ml-10 text-slate-50">Frontend</h1>
-            <div id="headingdiv2" className="w-fit">
-              <h1 id="heading2" className="text-[4vw] font-semibold uppercase opacity-60 leading-none ml-40 text-slate-50 ">Developer</h1>
-=======
             <h1 className="text-[5vw] font-semibold uppercase opacity-60 leading-none ml-8 lg:ml-10 text-slate-100">Frontend</h1>
             <div id="headingdiv2" className="w-fit">
               <h1 id="heading2" className="text-[5vw] font-semibold uppercase opacity-60 leading-none ml-40 text-slate-50">Developer</h1>
->>>>>>> 19a50227fe9c55dfd3d073945bdb7908c4e26b23:src/components/Hero.jsx
               <h4 id="text" className="text-right opacity-60 text-sm uppercase text-white">lives in Lahore</h4>
             </div>
             <div className='flex flex-wrap gap-5 hover:cursor-pointer'>
