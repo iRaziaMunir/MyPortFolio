d01eefd14bf310789c61e15dd0e7470948a8eef3
iRaziMunir <80644602+iRaziaMunir@users.noreply.github.com>
2024-05-21 13:58:05 +0500

    navLinks created with id

---
+++ src/components/navLinks.jsx
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { id: 'hero', title: 'Home', path: 'hero' },
+  { id: 'about', title: 'About', path: 'about' },
+  { id: 'skills', title: 'Skills', path: 'skills' },
+  { id: 'projects', title: 'Projects', path: 'projects' },
+  { id: 'contact', title: 'Contact', path: 'contact' },
+];
